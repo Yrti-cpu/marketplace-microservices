871cc0b3ec88a12a38a45789e6c3b72d92e88ac9
Danila Dotsenko <dadotsenko04@gmail.com>
2025-04-08 12:45:43 +0300

    Initial commit

---
+++ src/main/java/org/yrti/ventory/MarketplaceInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package org.yrti.ventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yrti/ventory/MarketplaceInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yrti.ventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
