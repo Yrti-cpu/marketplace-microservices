8d982556a0a6e7671e6b2b1865d6c1feda2d6519
Danila Dotsenko <dadotsenko04@gmail.com>
2025-04-22 11:40:22 +0300

    операции с деньгами - BigDecimal

--- marketplace-payment-service/src/main/java/org/yrti/payment/dto/PaymentRequest.java
+++ marketplace-payment-service/src/main/java/org/yrti/payment/dto/PaymentRequest.java
@@ -2,9 +2,11 @@ package org.yrti.payment.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class PaymentRequest {
     private Long orderId;
     private Long userId;
-    private Double amount;
+    private BigDecimal amount;
 }
