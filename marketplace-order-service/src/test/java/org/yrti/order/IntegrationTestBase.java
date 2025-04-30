1a8033956181c3f1d4efa4bb5d1c4361d9ef598e
Danila Dotsenko <dadotsenko04@gmail.com>
2025-04-30 18:52:33 +0300

    интеграционный тест на H2 для совместимости

--- marketplace-order-service/src/test/java/org/yrti/order/IntegrationTestBase.java
+++ marketplace-order-service/src/test/java/org/yrti/order/IntegrationTestBase.java
@@ -1,16 +1,13 @@
 package org.yrti.order;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.yrti.order.containers.PostgresTestContainer;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public abstract class IntegrationTestBase {
-
-    @BeforeAll
-    static void setUp() {
-        PostgresTestContainer.getInstance().start();
+    @Test
+    void contextLoads() {
     }
 }
--- marketplace-order-service/src/test/java/org/yrti/order/OrderRepositoryIntegrationTest.java
+++ marketplace-order-service/src/test/java/org/yrti/order/OrderRepositoryIntegrationTest.java
@@ -1,21 +1,21 @@
 package org.yrti.order;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.yrti.order.dao.OrderRepository;
 import org.yrti.order.model.Order;
 import org.yrti.order.model.OrderStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 class OrderRepositoryIntegrationTest extends IntegrationTestBase {
 
     @Autowired
     private OrderRepository orderRepository;
 
     @Test
-    @DisplayName("Сохраняем заказ и читаем его обратно")
     void saveAndFind() {
         Order order = Order.builder()
                 .userId(123L)
@@ -25,6 +25,7 @@ class OrderRepositoryIntegrationTest extends IntegrationTestBase {
         Order saved = orderRepository.save(order);
         Order found = orderRepository.findById(saved.getId()).orElseThrow();
 
+        assertThat(found.getId()).isNotNull();
         assertThat(found.getUserId()).isEqualTo(123L);
         assertThat(found.getStatus()).isEqualTo(OrderStatus.NEW);
     }
