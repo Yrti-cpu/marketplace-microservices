b1d6f0a76f7b5490b16b7b87421487fb7c7ff5f4
Danila Dotsenko <dadotsenko04@gmail.com>
2025-05-02 16:42:40 +0300

    AUTHSERVICE-1: 1) добавил новое поле для таблицы users 2) добивил новый модуль

---
+++ marketplace-auth-service/src/main/java/org/yrti/auth/MarketplaceAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package org.yrti.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceAuthServiceApplication.class, args);
+	}
+
+}
---
+++ marketplace-auth-service/src/test/java/org/yrti/auth/MarketplaceAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yrti.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
