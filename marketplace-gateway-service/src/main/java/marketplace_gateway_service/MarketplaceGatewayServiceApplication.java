e03135d430c2956785cb0bd8db97f3ba588423cb
Danila Dotsenko <i923b09@voenmeh.ru>
2025-05-04 17:05:13 +0300

    GATEWAYSERVICE-1: добавил новый модуль, базово настроил докер файл, докер-компоуз, добавил eureka для регистрации микросервисов (на примере inventory-service)

---
+++ marketplace-gateway-service/src/main/java/marketplace_gateway_service/MarketplaceGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package marketplace_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceGatewayServiceApplication.class, args);
+	}
+
+}
