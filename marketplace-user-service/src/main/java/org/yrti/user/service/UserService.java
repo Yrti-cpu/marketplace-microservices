561d1715fb2158e8856172815165bd000e59311a
Danila Dotsenko <i923b09@voenmeh.ru>
2025-05-08 19:25:07 +0300

    STUBS-2: user-service больше не заглушка, данные берет из бд, товар имеет внешний ключ на продавца

--- marketplace-user-service/src/main/java/org/yrti/user/service/UserService.java
+++ marketplace-user-service/src/main/java/org/yrti/user/service/UserService.java
@@ -2,9 +2,11 @@ package org.yrti.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.yrti.user.dao.UserRepository;
 import org.yrti.user.dto.UserResponse;
+import org.yrti.user.model.User;
 
 @Slf4j
 @Service
@@ -13,13 +15,16 @@ public class UserService {
 
   private final UserRepository userRepository;
 
-//    public Optional<User> getUserById(Long id) {
-//        return userRepository.findById(id);
-//    }
+  public User getCurrentUser() {
+    String email = SecurityContextHolder.getContext().getAuthentication().getName();
+    return userRepository.findByEmail(email)
+        .orElseThrow(() -> new RuntimeException("Пользователь не найден"));
+  }
 
   public UserResponse getUserById(Long id) {
     log.debug("Запрос профиля клиента: userId={}", id);
-    // Заглушка
-    return new UserResponse(id, "testuser" + id + "@mail.com", "Test User " + id);
+    User user = userRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Клиент с id: " + id + " не найден"));
+    return new UserResponse(id, user.getName(), user.getEmail());
   }
 }
